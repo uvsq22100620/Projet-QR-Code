4b5fb36f6d58f7bbbcc6901e906f6d638ff35a90
Laura LEFEVRE <laura.lefevre@ens.uvsq.fr>
2022-04-05 20:05:12 +0200

    modif nom

--- projet_GR_Code.py
+++
@@ -1,7 +0,0 @@
-######################################### 
-# LDDBI
-# LE CORRE Camille
-# COUSTILLAS Laurédane
-# LEFEVRE Laura
-# https://github.com/uvsq22100620/Projet-QR-Code.git
-# #########################################
